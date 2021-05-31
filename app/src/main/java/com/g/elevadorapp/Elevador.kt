ec513425fd4a79d9b09b23a8196291f9f63761a9
Mirian Fonkam <mirian.fonkam@aun.edu.ng>
2021-05-31 19:22:24 -0300

    2nd commit bug fixes

--- app/src/main/java/com/g/elevadorapp/Elevador.kt
+++ app/src/main/java/com/g/elevadorapp/Elevador.kt
@@ -1,24 +1,26 @@
 package com.g.elevadorapp
 
-class Elevador(val capacidade : Int = 1, val qty_andar : Int = 1, var andar_atual : Int = 0, var qty_pessoas : Int = 0){
+class Elevador(val capacidade : Int = 1, val total_andares : Int = 1, var andar_atual : Int = 0, var qty_pessoas : Int = 0){
 
-    private fun checarCapacidade() : Boolean {
+    fun checarCapacidade() : Boolean {
         return this.qty_pessoas + 1 <= this.capacidade
     }
 
-    private fun updateAndar (num: Int) {
-        if (num <= this.qty_andar) {
+    fun updateAndar (num: Int) {
+        if (num <= this.total_andares) { // in 0 .. qty_tanday
             this.andar_atual = num
         }
     }
 
-    private fun addPessoa() {
+    fun addPessoa() {
         if (this.checarCapacidade()) {
             this.qty_pessoas += 1
         }
     }
 
-    private fun tirarPessoa() {
-        this.qty_pessoas -= 1
+    fun tirarPessoa() {
+        if (qty_pessoas > 0) {
+            this.qty_pessoas -= 1
+        }
     }
 }
--- app/src/main/java/com/g/elevadorapp/MainActivity.kt
+++ app/src/main/java/com/g/elevadorapp/MainActivity.kt
@@ -9,23 +9,25 @@ import com.google.android.material.button.MaterialButton
 class MainActivity : AppCompatActivity() {
 
     val ev = Elevador(5,12, 5, 2 )
-    //val btnEntrar = findViewById<MaterialButton>(R.id.btn_entrar)
-//    val evQtyPessoa = findViewById<TextView>(R.id.ev_result)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        btnEntrar.setOnClickListener{
-//            ev.addPessoa()
-//            evQtyPessoa.text =  "${ev.qty_pessoas}/5 \n pessoas no \n elevador"
-//        }
+        val btnEntrar = findViewById<MaterialButton>(R.id.btn_entrar)
+        val evQtyPessoa = findViewById<TextView>(R.id.ev_result)
+        val btnSair = findViewById<MaterialButton>(R.id.btn_sair)
 
-        //btnSair.setOnClickListener{
-//            ev.tirarPessoa()
-//            evQtyPessoa.text =  "${ev.qty_pessoas}/5 \n pessoas no \n elevador"
-//        }
+        btnEntrar.setOnClickListener {
+            ev.addPessoa()
+            evQtyPessoa.text = "${ev.qty_pessoas}/5 \n pessoas no \n elevador"
+        }
+
+        btnSair.setOnClickListener{
+            ev.tirarPessoa()
+            evQtyPessoa.text =  "${ev.qty_pessoas}/5 \n pessoas no \n elevador"
+        }
 
     //    btnAndar.setOnClickListener{
 //            ev.updateAndar(numInput)
